d847e79cc6cd21bf53c7332c87ae1c52543730b4
John-George <johngeorgesample@gmail.com>
2017-04-05 18:26:54 -0400

    talking 'bout myBoard

--- Board.java
+++ Board.java
@@ -2,21 +2,24 @@ import java.util.Arrays;
 import java.util.*;
 
 public class Board {
-	
-	
-	
+	private int[][] myBoard;
 	
 	Board() {
 		//0 = open
 		//1 = peg
 		//2 = shadow realm
-		int[][] board = { {0, 2, 2, 2, 2, 2},
+		int[][] myBoard = new int[][]{ 
+						  {0, 2, 2, 2, 2, 2},
 						  {0, 0, 2, 2, 2, 2},
 						  {0, 0, 0, 2, 2, 2},
 						  {0, 0, 0, 0, 2, 2},
 						  {0, 0, 0, 0, 0, 2},
 						  {0, 0, 0, 0, 0, 0},
 						};		
+	}
+	
+	public void loadBoard(char a, int row, int col) {
+		myBoard [row][col] = a;
 	}
 	
 }
