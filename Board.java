6e31ab9404557f702a5b0812ac3ed9b83a07c781
John-George <johngeorgesample@gmail.com>
2017-04-05 17:54:51 -0400

    Created Board.java and Board constructor

---
+++ Board.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.*;
+
+public class Board {
+	
+	
+	
+	
+	Board() {
+		int[][] board = { {0, 0, 0, 0, 0, 0},
+						  {0, 0, 0, 0, 0, 0},
+						  {0, 0, 0, 0, 0, 0},
+						  {0, 0, 0, 0, 0, 0},
+						  {0, 0, 0, 0, 0, 0},
+						  {0, 0, 0, 0, 0, 0},
+						};		
+	}
+	
+}
