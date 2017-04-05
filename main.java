326eccd91864d9139e907c80e8032c35eb782da6
andyvilsack <andy.vilsack@gmail.com>
2017-04-05 18:07:54 -0400

    added main

---
+++ main.java
@@ -0,0 +1,34 @@
+/*
+ * Andrew Vilsack
+ * John-George Sample
+ * 
+ */
+
+
+import java.util.Scanner;
+public class main 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner scanner = new Scanner(System.in);
+		String input = "";
+		System.out.println("Please enter values for the starting positions:");
+		for(int i=0;i<6;i++)
+		{
+			System.out.println("Enter for line "+(i+1));
+			input = scanner.nextLine();
+		}
+		
+		for(int j=0;j<6;j++)
+		{
+			System.out.println("enter for Line "+(j+1));
+			input = scanner.nextLine();
+		}
+		
+		
+		
+		
+	}
+
+}
